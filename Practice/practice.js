04d837710d66f55d1a37666772ad9d2987c08ec0
LonelyFirefly <Itisperfectworld@gmail.com>
2022-11-13 11:53:47 +0400

    exponentiate task

--- Practice/practice.js
+++ Practice/practice.js
@@ -1,6 +1,16 @@
 "use strict";
 
-function doNothing() {
-	return;
+alert("Enter a number you want to exponentiate");
+let x = prompt();
+alert("Enter a power");
+let n = prompt();
+
+function pow(x, n) {
+	if (x % 1 == 0 && n % 1 == 0 && x >= 1 && n >= 1) {
+		return x ** n;
+	} else {
+		return alert("not working");
+	}
 }
-alert(doNothing() === undefined);
+
+alert(`Result is ${pow(x, n)}`);
