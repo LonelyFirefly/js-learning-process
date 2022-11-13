0ec06f613f61af17e8dc1ffdc157e98f4506ca7c
LonelyFirefly <Itisperfectworld@gmail.com>
2022-11-13 20:47:14 +0400

    add

--- Practice/practice.js
+++ Practice/practice.js
@@ -1,16 +1,3 @@
 "use strict";
 
-alert("Enter a number you want to exponentiate");
-let x = prompt();
-alert("Enter a power");
-let n = prompt();
-
-function pow(x, n) {
-	if (x % 1 == 0 && n % 1 == 0 && x >= 1 && n >= 1) {
-		return x ** n;
-	} else {
-		return alert("not working");
-	}
-}
-
-alert(`Result is ${pow(x, n)}`);
+alert(null === undefined);
