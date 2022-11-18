47b69bbf9121ea8f7d351d45e0c3360a4ede8828
LonelyFirefly <Itisperfectworld@gmail.com>
2022-11-18 13:49:52 +0400

    changes

--- Practice/practice.js
+++ Practice/practice.js
@@ -1,3 +1,22 @@
 "use strict";
 
-let use;
+let menu = {
+	width: 200,
+	height: 300,
+	title: "My menu",
+};
+
+multiplyNumeric(menu);
+
+function multiplyNumeric(menu) {
+	for (const key in menu) {
+		if (!NaN) menu[key] *= 2;
+		else {
+			continue;
+		}
+	}
+}
+
+alert(menu.width);
+alert(menu.height);
+alert(menu.title);
