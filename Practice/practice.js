3229e08f334631821b7e87854bcbc09d416dc90c
LonelyFirefly <Itisperfectworld@gmail.com>
2022-11-20 16:38:18 +0400

    add

--- Practice/practice.js
+++ Practice/practice.js
@@ -1,19 +1,5 @@
 "use strict";
 
-let menu = {
-	width: 200,
-	height: 300,
-	title: "My menu",
-};
+let id = Symbol("id");
 
-multiplyNumeric(menu);
-
-function multiplyNumeric(menu) {
-	for (const key in menu) {
-		if (typeof menu[key] == "number") menu[key] *= 2;
-	}
-}
-
-alert(menu.width);
-alert(menu.height);
-alert(menu.title);
+alert(id.description);
