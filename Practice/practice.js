b5f09bb1bff6b541050ad6d4b9e13c9e75d1cdd4
LonelyFirefly <Itisperfectworld@gmail.com>
2022-11-24 22:01:48 +0400

    changes

--- Practice/practice.js
+++ Practice/practice.js
@@ -1,8 +1,20 @@
 "use strict";
 
-array = [1, -2, 3, 4, -9, 6];
+function getMaxSubSum(array) {
+	let maxSum = 0;
+	let partialSum = 0;
 
-for (let index = 0; index < array.length; index++) {
-	if (array[index] > array[index + 1]) {
+	for (let index = 0; index < array.length; index++) {
+		partialSum += array[index];
+		if (partialSum < 0) partialSum = 0;
+		maxSum = Math.max(partialSum, maxSum);
 	}
+	alert(maxSum);
 }
+
+getMaxSubSum([-1, 2, 3, -9]); // 5
+getMaxSubSum([-1, 2, 3, -9, 11]); // 11
+getMaxSubSum([-2, -1, 1, 2]); // 3
+getMaxSubSum([100, -9, 2, -3, 5]); // 100
+getMaxSubSum([1, 2, 3]); // 6
+getMaxSubSum([-1, -2, -3]); // 0
