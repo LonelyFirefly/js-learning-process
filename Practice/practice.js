41e565e3b79704bf103c6a7cb6e85093a3420f32
LonelyFirefly <Itisperfectworld@gmail.com>
2022-12-01 16:22:57 +0400

    arrays ex

--- Practice/practice.js
+++ Practice/practice.js
@@ -1,23 +1,16 @@
 "use strict";
 
-let powerCalc = new Calculator();
-powerCalc.addMethod("*", (a, b) => a * b);
-powerCalc.addMethod("/", (a, b) => a / b);
-powerCalc.addMethod("**", (a, b) => a ** b);
-
-let Calculator = calculate(str) {
-	str = str.split(" ");
-	return +str[0] + +str[2];
+function groupById(users) {
+	return users.reduce((obj, value) => {
+		obj[value.id] = value;
+		return obj;
+	}, {});
 }
 
-function addMethod(operatorFromString, func) {
-	operatorFromString = {
-		"+": (a, b) => a + b,
-		"/": (a, b) => a / b,
-		"*": (a, b) => a * b,
-		"**": (a, b) => a ** b,
-	};
-}
+let users = [
+	{ id: "john", name: "John Smith", age: 20 },
+	{ id: "ann", name: "Ann Smith", age: 24 },
+	{ id: "pete", name: "Pete Peterson", age: 31 },
+];
 
-let result = powerCalc.calculate("2 ** 3");
-alert(result); // 8
+let usersById = groupById(users);
