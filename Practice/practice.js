fd30c1620703bc6f4fb1e65e4b1e9357cd7efb16
LonelyFirefly <Itisperfectworld@gmail.com>
2022-12-14 11:41:12 +0400

    add

--- Practice/practice.js
+++ Practice/practice.js
@@ -1,39 +1,5 @@
 "use strict";
 
-// function printList(list) {
-// 	if (list.next) {
-// 		printList(list.next); // делаем то же самое для остальной части списка
-// 	}
+let str = ["H", "e", "l", "l", "o"];
 
-// 	alert(list.value); // выводим текущий элемент
-// }
-
-function printList(list) {
-	let temporaryVariable = list;
-	let arr = [];
-
-	while (temporaryVariable) {
-		arr.push(temporaryVariable.value);
-		temporaryVariable = temporaryVariable.next;
-	}
-
-	for (let index = arr.length - 1; 0 <= index; index--) {
-		alert(arr[index]);
-	}
-}
-
-let list = {
-	value: 1,
-	next: {
-		value: 2,
-		next: {
-			value: 3,
-			next: {
-				value: 4,
-				next: null,
-			},
-		},
-	},
-};
-
-printList(list);
+alert(...str);
