c9f2a521e1be34e50b5dc969eedaa6dbd5034caa
LonelyFirefly <Itisperfectworld@gmail.com>
2022-12-15 13:22:26 +0400

    add

--- Practice/practice.js
+++ Practice/practice.js
@@ -1,15 +1,4 @@
 "use strict";
 
-function makeCounter() {
-	let count = 0;
-
-	return function () {
-		return count++;
-	};
-}
-
-let counter = makeCounter();
-let count = 10;
-alert(counter());
-alert(counter());
-alert(counter());
+function inBetween(a, b) {}
+function inArray([]) {}
