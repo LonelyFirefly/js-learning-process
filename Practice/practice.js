0df9d779d14ea488378cb60f7fc1eba5460442ef
LonelyFirefly <Itisperfectworld@gmail.com>
2022-12-19 18:24:44 +0400

    add

--- Practice/practice.js
+++ Practice/practice.js
@@ -1,25 +1,10 @@
 "use strict";
 
-function f(a) {
-	alert(a);
+function f() {
+	alert(this.name);
 }
 
-function throttle(f, ms) {
-	let count = 0;
-	count++;
-	let countSum = [];
-	countSum.push(count);
-	let isCoolDown = false;
-	return function () {
-		if (isCoolDown) return;
-		f.apply(this, arguments);
-		isCoolDown = true;
-		setTimeout(() => (setTimeout = false), ms);
-	};
-	return f.apply(this, arguments);
-}
+f = f.bind({ name: "Dan" });
+f.bind({ name: "Leo" });
 
-let f1000 = throttle(f, 1000);
-f1000(1); // показывает 1
-f1000(2); // (ограничение, 1000 мс ещё нет)
-f1000(3); // (ограничение, 1000 мс ещё нет)
+f();
