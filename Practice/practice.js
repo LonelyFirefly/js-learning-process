a27df03b46777e5fad99700bf150aaeafb779338
LonelyFirefly <Itisperfectworld@gmail.com>
2022-12-20 13:57:48 +0400

    study

--- Practice/practice.js
+++ Practice/practice.js
@@ -1,6 +1,20 @@
 "use strict";
 
-let Math = {
-	PI: 19,
-};
-Math.PI(s);
+function User(name, birthday) {
+	this.name = name;
+	this.birthday = birthday;
+	Object.defineProperty(this, "age", {
+		get() {
+			let todayYear = new Date().getFullYear();
+			return todayYear - this.birthday.getFullYear();
+		},
+		set(value) {
+			this._age = value;
+		},
+	});
+}
+
+let john = new User("Dan", new Date(2002, 4, 9));
+john.age = 23;
+alert(john.age);
+alert(john.birthday);
