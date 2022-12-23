81a936d953fae93197bd12a02d96d4004bb442b7
LonelyFirefly <Itisperfectworld@gmail.com>
2022-12-23 17:29:47 +0400

    ad

--- Practice/practice.js
+++ Practice/practice.js
@@ -1,38 +1,21 @@
 "use strict";
 
-class Animal {
-	constructor(name) {
-		this.speed = 0;
-		this.name = name;
+class CoffeeMachine {
+	_waterAmount = 0;
+
+	constructor(power) {
+		this.power = power;
 	}
-	run(speed) {
-		this.speed = speed;
-		alert(`${this.name} runs with a speed of ${this.speed}.`);
+
+	get waterAmount() {
+		return this._waterAmount;
 	}
-	stop() {
-		this.speed = 0;
-		alert(`${this.name} doesn't move.`);
+
+	set waterAmount(value) {
+		if (value < 0) throw new Error("Negative amount of water");
+		this._waterAmount = value;
 	}
 }
 
-class Rabbit extends Animal {
-	constructor(name, earLength) {
-		super(name);
-		this.speed = 0;
-		this.earLength = earLength;
-	}
-
-	hide() {
-		alert(`${this.name} is hiding`);
-	}
-
-	stop() {
-		super.stop();
-		this.hide();
-	}
-}
-
-let rabbit = new Rabbit("White Rabbit", 10);
-
-alert(rabbit.name);
-alert(rabbit.earLength);
+let coffeeMachine = new CoffeeMachine(100);
+coffeeMachine.waterAmount = -200;
