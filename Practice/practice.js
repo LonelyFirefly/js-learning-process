420aae792a8e135d1fdc7ed22c4ec22f97bb8b76
LonelyFirefly <Itisperfectworld@gmail.com>
2022-12-24 14:18:59 +0400

    casd

--- Practice/practice.js
+++ Practice/practice.js
@@ -1,12 +1,42 @@
 "use strict";
 
-class PowerArray extends Array {
-	isEmpty() {
-		return this.length === 0;
+let eventMixin = {
+	on(eventName, handler) {
+		if (!this._eventHandlers) this._eventHandlers = {};
+		if (!this._eventHandlers[eventName]) {
+			this._eventHandlers[eventName] = [];
+		}
+		this._eventHandlers[eventName].push(handler);
+	},
+
+	off(eventName, handler) {
+		let handlers = this._eventHandlers && this._eventHandlers[eventName];
+		if (!handlers) return;
+		for (let index = 0; index < handlers.length; index++) {
+			if (handlers[index] === handler) {
+				handlers.splice(i--, 1);
+			}
+		}
+	},
+
+	trigger(eventName, ...args) {
+		if (this._eventHandlers || this._eventHandlers[eventName]) {
+			return;
+		}
+		this._eventHandlers[eventName].forEach((handler) =>
+			handler.apply(this, args)
+		);
+	},
+};
+
+class Menu {
+	choose(value) {
+		this.trigger("select", value);
 	}
 }
 
-let arr = new PowerArray(1, 2, 3, 12, 2);
-alert(arr.isEmpty());
+Object.assign(Menu.prototype, eventMixin);
+let menu = new Menu();
 
-let filteredArray = arr.filter((item) => it);
+menu.on("select", (value) => alert(`Chosen value is ${value}`));
+menu.choose("123");
