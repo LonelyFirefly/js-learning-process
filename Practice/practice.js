7fd3f605cd9170597521a7cc100b0ed7d6fb2da0
LonelyFirefly <Itisperfectworld@gmail.com>
2023-01-04 10:50:38 +0400

    change

--- Practice/practice.js
+++ Practice/practice.js
@@ -1,16 +1,5 @@
 "use strict";
 
-let list = document.createElement("ul");
-document.body.append(list);
+document.body.classList.add("fuck");
 
-while (true) {
-	let data = prompt("content?");
-
-	if (!data) {
-		break;
-	}
-
-	let li = document.createElement("li");
-	li.textContent = data;
-	list.append(li);
-}
+document.querySelector(".what").style.MozBorderImage = none;
