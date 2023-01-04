1862345c5399737459c9f081401a62249f0ab992
LonelyFirefly <Itisperfectworld@gmail.com>
2023-01-04 23:07:27 +0400

    push

--- Practice/practice.js
+++ Practice/practice.js
@@ -1,5 +1,16 @@
 "use strict";
 
-document.body.classList.add("fuck");
+function showNotification({ top, right, html, className }) {
+	let notification = document.createElement("div");
+	notification.classList.add("notification");
+	notification.style.top = top + "px";
+	notification.style.right = right + "px";
+	notification.innerHTML = html;
+	notification.classList.add(className);
+	document.body.append(notification);
+	setTimeout(() => {
+		notification.remove();
+	}, 1500);
+}
 
-document.querySelector(".what").style.MozBorderImage = none;
+showNotification();
